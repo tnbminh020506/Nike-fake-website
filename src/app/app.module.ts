2ba8d6f5cbdb47936bc7489f11d9038e498f31e3
Truong Nguyen Binh Minh <tnbminh@gmail.com>
2025-02-10 10:09:38 +0700

    delete first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";  
 
-import { provideRouter, RouterModule, RouterOutlet, withComponentInputBinding } from "@angular/router";
+import { provideRouter, RouterOutlet, withComponentInputBinding } from "@angular/router";
 import { RouterLink } from "@angular/router";
 import { RouterLinkActive } from "@angular/router";
 
@@ -9,10 +9,9 @@ import { FormsModule } from "@angular/forms";
 import { provideHttpClient } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { HomePageComponent } from "./home-page/home-page.component";
 import { FirstPageComponent } from "./shoes-page/shoes-page.component";
 import { DetailProductComponent } from "./detail-product/detail-product.component";
-import { HeaderComponent } from "./3-header/header.component";
+import { HeaderComponent } from "./header/header.component";
 import { ShopCartComponent } from "./shop-cart/shop-cart.component";
 
 import { ProductService } from "./typescript/product.service";
@@ -23,7 +22,6 @@ import { routes } from "./app.routes";
 @NgModule({
     declarations: [
         AppComponent,
-        HomePageComponent,
         FirstPageComponent,
         DetailProductComponent,
         HeaderComponent,
